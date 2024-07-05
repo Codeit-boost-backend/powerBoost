35c5fadb4f724c1d3e25a3186579a8082e3fec72
haeun228 <123haeun@naver.com>
2024-07-05 13:23:48 +0900

    Add 글 작성/수정/삭제

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
 const express = require('express')
+
 const app = express()
+app.use(express.json())
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+let posts = [];
 
-app.listen(3000)
+// 전체 글 조회
+app.get('/posts', function (req, res) {
+  res.send(posts)
+});
+
+// 특정 글 조회
+app.get('/posts/:id', function (req, res) {
+  const id = Number(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  res.send(post);
+});
+
+// 글 작성 
+app.post('/posts', (req, res) => {
+  const newPost = req.body;
+  const ids = posts.map((post) => post.id);
+  let postId;
+  if (ids.length===0){
+    postId = 0
+  } else {
+    postId = Math.max(...ids) + 1;
+  }
+  newPost.id = postId;
+  newPost.comments = {};
+  newPost.likes = 0;
+
+  posts.push(newPost);
+  res.status(201).send(newPost);
+});
+
+// 글 수정
+app.patch('/posts/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    Object.keys(req.body).forEach((key) => {
+      post[key] = req.body[key];
+    });
+    res.send(post);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
+});
+
+// 글 삭제
+app.delete('/posts/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const idx = posts.findIndex((post) => post.id === id);
+  if (idx>=0) {
+    posts.splice(idx, 1);
+    res.sendStatus(204);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
